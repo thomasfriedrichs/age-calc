475138a660173127a5edd24b1862029524c82e7d
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-04-25 11:44:50 -0700

    add failing test

--- __tests__/venus.test.js
+++ __tests__/venus.test.js
@@ -6,4 +6,10 @@ describe ('Venus', () => {
     const newVenus = new Venus();
     expect(newVenus.year).toEqual(0.62);
   });
+
+  test('it should return my age in venus years', () => {
+    const newVenus = new Venus();
+    newVenus.ageCalc();
+    expect(venusAge).toEqual(40);
+  });
 });
