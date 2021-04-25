0f20e6c3ab07c39d4d52ccc9557c4f6d5433bb74
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-04-25 08:53:09 -0700

    add calculator object

---
+++ src/js/calculator.js
@@ -0,0 +1,10 @@
+// import Mercury from 'mercury.js';
+// import Venus from 'venus.js';
+// import Mars from 'mars.js';
+// import Jupiter from 'jupiter.js';
+
+export default class Calculator {
+  constructor(age) {
+    this.age = age;
+  }
+}
