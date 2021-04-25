b6c295f2171310d653023c19a7e6a850cb2afbd6
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-04-25 12:55:37 -0700

    add yearsleftcalculator

--- src/js/mars.js
+++ src/js/mars.js
@@ -1,11 +1,17 @@
 export default class Mars {
-  constructor(myAge) {
+  constructor(myAge, lifeExpectancy) {
     this.marsYear = 1.88;
     this.myAge = myAge;
     this.marsAge = 0;
+    this.yearsLeft;
+    this.lifeExpectancy = lifeExpectancy;
   }
 
   ageCalc() {
     return this.marsAge += Math.floor((this.myAge / this.marsYear));
+  }
+
+  yearsLeftCalculator() {
+    return this.yearsLeft = (this.lifeExpectancy - this.marsAge);
   }
 }
